d31ecb114989b84cff9441bef5d513e28057bb66
Alejandro Hurtado Chacñama <ale.1010@live.com>
2020-03-22 18:43:34 -0500

    Modify index

--- hackactivista/settings/develop.py
+++ hackactivista/settings/develop.py
@@ -10,10 +10,20 @@ with open(fileConfig) as data_file:
     dataConfig = json.load(data_file)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': dataConfig.get('mongo-connection').get('database_name'),
+        'HOST': dataConfig.get('mongo-connection').get('host'),
+        'PORT': dataConfig.get('mongo-connection').get('port'),
+        'USER': dataConfig.get('mongo-connection').get('user'),
+        'PASSWORD': dataConfig.get('mongo-connection').get('password')
     }
 }
 
