d30efffcdb9175dae4eeeb3fd71d3d1c7e4d4d35
Alejandro Hurtado Chacñama <ale.1010@live.com>
2020-04-08 20:17:02 -0500

    Modify app covid19

--- applications/covid19/views.py
+++ applications/covid19/views.py
@@ -1,31 +1,28 @@
 from django.shortcuts import render
-from braces.views import (LoginRequiredMixin, 
-	PermissionRequiredMixin)
-from django.views.generic import (ListView, UpdateView, 
-	DeleteView, CreateView, TemplateView)
 import requests
+import json
+from django.conf import settings
+from braces.views import (LoginRequiredMixin, PermissionRequiredMixin)
+from django.views.generic import (ListView, UpdateView, 
+								DeleteView, CreateView, TemplateView)
 
 # Create your views here.
-
-
 class DiagnosisCovid19(LoginRequiredMixin, TemplateView):
     template_name = 'covid19/diagnosis_covid_19.html'
 
+class APIClient:
+    def __init__(self):
+        self.covidnet_endpoint = settings.COVIDNET_ENDPOINT
+        self.covidnet_apikey = settings.COVIDNET_API_KEY
+    def covidnet_post(self, path_file):
+        url = self.covidnet_endpoint
+        files = {'file': open(path_file, 'rb')}
+        headers = {'apikey': self.covidnet_apikey}
+        resp = requests.post(url, files=files, headers=headers)
+        resp_json = json.loads(resp.text)
+        print(resp_json)
+        return resp_json.get('result')
 
 def api_diagnosis_covid19():
-
-	url = "http://ec2-18-212-109-235.compute-1.amazonaws.com/covidnet/imgxrays"
-
-	payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data;\
-	 		name=\"file\"; filename=\"prueba1.jpg\"\r\n\
-	 		Content-Type: image/jpeg\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
-	headers = {
-	    'content-type': "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
-	    'apikey': settings.api_diagnosis_covid19.get('apikey'),
-	    'cache-control': "no-cache",
-	    'postman-token': "20659d89-7f2a-c2a8-563e-8a5805fede03"
-	    }
-
-	response = requests.request("POST", url, data=payload, headers=headers)
-
-	print(response.text)
+	ac = APIClient()
+	print(ac.covidnet_post('/home/alejandro/Desktop/prueba1.jpg'))
